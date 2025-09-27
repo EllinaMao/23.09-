05061f639a97d83eb88eceaa1ce167f2e2d50e25
Olga <xotaoshi@gmail.com>
2025-09-28 01:07:04 +0300

    finish

--- Task 4 Child/Program.cs
+++ Task 4 Child/Program.cs
@@ -22,14 +22,3 @@ namespace Task_4_Child
         }
     }
 }
-//Unhandled exception. System.IO.FileNotFoundException: Could not find file 'D:\lessons\СисПрограмирование\23.09 Процеси2\Task 4 Child\bin\Debug\net8.0\text.txt'.
-//File name: 'D:\lessons\СисПрограмирование\23.09 Процеси2\Task 4 Child\bin\Debug\net8.0\text.txt'
-//   at Microsoft.Win32.SafeHandles.SafeFileHandle.CreateFile(String fullPath, FileMode mode, FileAccess access, FileShare share, FileOptions options)
-//   at Microsoft.Win32.SafeHandles.SafeFileHandle.Open(String fullPath, FileMode mode, FileAccess access, FileShare share, FileOptions options, Int64 preallocationSize, Nullable`1 unixCreateMode)
-//   at System.IO.Strategies.OSFileStreamStrategy..ctor(String path, FileMode mode, FileAccess access, FileShare share, FileOptions options, Int64 preallocationSize, Nullable`1 unixCreateMode)
-//   at System.IO.Strategies.FileStreamHelpers.ChooseStrategyCore(String path, FileMode mode, FileAccess access, FileShare share, FileOptions options, Int64 preallocationSize, Nullable`1 unixCreateMode)
-//   at System.IO.FileStream..ctor(String path, FileMode mode, FileAccess access)
-//   at Task_4_Child.Program.Main(String[] args) in D:\lessons\СисПрограмирование\23.09 Процеси2\Task 4 Child\Program.cs:line 13
-
-//D:\lessons\СисПрограмирование\23.09 Процеси2\Task 4 Child\bin\Debug\net8.0\Task 4 Child.exe (process 23204) exited with code -532462766 (0xe0434352).
-//Press any key to close this window . . .
