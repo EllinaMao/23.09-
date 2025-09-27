18025ab0ceb041cc3942589988d371c268ed1a11
Olga <xotaoshi@gmail.com>
2025-09-27 18:13:39 +0300

    сохранить

---
+++ Task 4 Child/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task_4_Child
+{
+    internal class Program
+    {
+        /*Дочірній процес має відобразити кількість разів, скільки слово bicycle зустрічається у файлі.*/
+
+
+        static void Main(string[] args)
+        {
+            args = new string[] { "text.txt", "bicycle" };
+            FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string fileContent = sr.ReadToEnd();
+            sr.Close();
+            Regex pattern = new Regex($@"\b{args[1]}\b", RegexOptions.IgnoreCase);
+            MatchCollection matches = pattern.Matches(fileContent);
+            Console.WriteLine($"The word '{args[1]}' occurs {matches.Count} times in the file '{args[0]}'.");
+
+
+
+        }
+    }
+}
